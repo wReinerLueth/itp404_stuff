fc4401f0bf18937ef35f1899daded90a57c3fa2a
wReinerLueth <wlueth@usc.edu>
2017-11-22 16:46:44 -0800

    couldn't get the logic, but it changes with button clicks

--- app/components/reddit-post.js
+++ app/components/reddit-post.js
@@ -1,10 +1,19 @@
 import Ember from 'ember';
+import $ from 'jquery';
 
 export default Ember.Component.extend({
     actions: {
         changeScore(score){
-            console.log("reddit-post: " + score);
-            this.set('score', score);
+            let originalScore = this.get('val.data.score');
+            if(score > originalScore){
+                $('.fa fa-arrow-up').prop("onclick", null);
+                $('.fa fa-arrow-down').prop("onclick", 'down()');
+            }
+            else{
+                $('.fa fa-arrow-up').prop("onclick", 'up()');
+                $('.fa fa-arrow-down').prop("onclick", null);
+            }
+            this.set('val.data.score', score);
         }
     }
 });
